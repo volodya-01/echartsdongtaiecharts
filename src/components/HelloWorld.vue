4cce82901176e131534b0f34b1adc9a1d4ed2ebe
unknown <1909590616@qq.com>
2018-12-21 15:54:55 +0800

    dongtaiecharts

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <hello/>
+ <echart1/>
+ <echart2/>
+  </div>
+</template>
+
+<script>
+import hello from '@/components/hello'
+import echart1 from '@/components/echart1'
+import echart2 from '@/components/echart2'
+export default {
+  name: 'HelloWorld',
+  components:{
+    echart1,
+    echart2,
+    hello
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/echart2.vue
@@ -0,0 +1,138 @@
+<template>
+  <div>
+    <div id="echart2" ref="echart2"></div>
+    <div>{{show}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "echart2",
+  data() {
+    return {
+      aadata: [],
+      bbdata: [],
+      ccdata: [],
+      zydata: [],
+      show: []
+    };
+  },
+  mounted() {
+    var that = this;
+   /*  var e = [1, 2, 3, 4];
+    console.log(e.splice(e.indexOf(2), 1));
+    console.log(e); */
+    var aa = Math.random();
+    var cc = [];
+    for (var i = 0; i < 100; i++) {
+      that.aadata.push(aa);
+      that.bbdata.push(i);
+      cc.push(0);
+    }
+    cc.splice(cc.length - 1, 1, that.aadata[that.aadata.length - 1]);
+    that.ccdata = cc;
+    that.api();
+    setInterval(function() {
+      that.api();
+    }, 1000);
+  },
+ /*  watch: {
+    xdata(re) {
+      this.drawLine();
+    }
+  }, */
+  methods: {
+    api() {
+          var that = this;
+          var b =Math.random().toFixed(2);
+          that.show=b
+          that.aadata.shift();
+          /*   .shift() */
+          that.bbdata.shift();
+          that.aadata.push(b);
+          that.bbdata.push(b);
+          console.log(that.aadata);
+          console.log(that.aadata);
+          that.ccdata.splice(that.zydata.length - 1, 1, b);
+          console.log(that.zydata);
+          /* that.$set(that.xdata.shift()); */
+          that.drawLine();
+       
+    },
+    drawLine() {
+      var that = this;
+      var myChart = this.$echarts.init(this.$refs.echart2);
+      myChart.setOption({
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: that.bbdata
+        },
+        yAxis: {
+          type: "value"
+          /* max: 0.5 */
+        },
+        series: [
+          {
+            data: that.aadata,
+            type: "line",
+             animation: false,
+            smooth: true,
+            symbol: "none",
+            lineStyle: {
+              color: {
+                type: "linear",
+                /*   x: 0,
+    y: 0,
+    x2: 0,
+    y2: 1, */
+                colorStops: [
+                  {
+                    offset: 0,
+                    color: "#0eb92e" // 0% 处的颜色
+                  },
+                  {
+                    offset: 0.8,
+                    color: "#0eb92e" // 0% 处的颜色
+                  },
+                  {
+                    offset: 1,
+                    color: "#f0e242" // 100% 处的颜色
+                  }
+                ],
+                opacity: 0.4,
+                globalCoord: false // 缺省为 false
+              }
+            }
+          },
+          {
+            name: "最高气温",
+            barWidth: 2,
+            type: "bar",
+            data: that.ccdata,
+            animation: false,
+           /*  markPoint: {
+              animation: false,
+              symbol: "emptyCircle",
+              data: [{ type: "max" }],
+              symbolSize: 16,
+              itemStyle: {
+                normal: {
+                  label: {
+                    show: false
+                  }
+                }
+              }
+            } */
+          }
+        ]
+      });
+    }
+  }
+};
+</script>
+<style scoped>
+#echart2 {
+  width: 100%;
+  height: 400px;
+}
+</style>
